f0147841fd86deea3afef016d3740155ef8ab325
Samdanova <samdanova123@gmail.com>
2022-04-29 17:28:17 +0300

    Таблица слов

--- english/src/App.js
+++ english/src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 import TableOfWords from './assets/components/TableOfWords';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+import words from './json/words.json';
 
 
 function App() {
   return (
     <div className="App">
-     <TableOfWords></TableOfWords>
+          <table className="table">
+          <thead>
+                <tr className="table__columns">
+                    <th className="table__columns_item" colspan="1">English</th>
+                    <th className="table__columns_item" colspan="1">Transctiption</th>
+                    <th className="table__columns_item" colspan="1">Russian</th>
+                    <th className="table__columns_item" colspan="1">Tags</th>
+                    <th className="table__columns_item" colspan="1">Action</th>
+                </tr>
+            </thead>
+      {
+        words.map((word) => 
+        <TableOfWords
+        english={word.english}
+        transcription={word.transcription}
+        russian={word.russian}
+        tags={word.tags}
+        isSelected={word.isSelected}></TableOfWords>
+        )
+      }
+    </table>
     </div>
   );
 }
--- english/src/assets/components/TableOfWords.jsx
+++ english/src/assets/components/TableOfWords.jsx
@@ -1,46 +1,80 @@
 import React from 'react';
-import words from '../../json/words.json';
-import { Table, Space } from 'antd';
+// import words from '../../json/words.json';
+// import { Table, Space } from 'antd';
+import { Input } from 'antd';
+import { Button } from 'antd';
 
-const columns = [
-    {
-        title: 'English',
-        dataIndex: 'english',
-        key: 'english',
-    },
-    {
-        title: 'transcription',
-        dataIndex: 'transcription',
-        key: 'transcription',
-    },
-    {
-        title: 'Russian',
-        dataIndex: 'russian',
-        key: 'russian',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: () => (
-            <Space size="middle">
-                <button>Edit</button>
-                <button>Delete</button>
-            </Space>
-        ),
-    },
-];
+// const columns = [
+//     {
+//         title: 'English',
+//         dataIndex: 'english',
+//         key: 'english',
+//     },
+//     {
+//         title: 'transcription',
+//         dataIndex: 'transcription',
+//         key: 'transcription',
+//     },
+//     {
+//         title: 'Russian',
+//         dataIndex: 'russian',
+//         key: 'russian',
+//     },
+//     {
+//         title: 'Tags',
+//         key: 'tags',
+//         dataIndex: 'tags',
+//     },
+//     {
+//         title: 'Action',
+//         key: 'action',
+//         render: () => (
+//             <Space size="middle">
+//                 <button>Edit</button>
+//                 <button>Delete</button>
+//             </Space>
+//         ),
+//     },
+// ];
+
+
+// function TableOfWords(props) {
+//     return (
+//         <Table dataSource={props.english} columns={columns} />
+//     )
+// }
 
 
 function TableOfWords(props) {
     return (
-        // <Table columns={columns} dataSource={words} />
-        <Table dataSource={words} columns={columns} />
+        <tbody className="table__tbody">
+            <tr>
+                <td className="table__data">{props.isSelected
+                    ? <Input placeholder="Введите слово" />
+                    : props.english}</td>
+                <td className="table__data">{props.isSelected
+                    ? <Input placeholder="Введите транскрипцию" />
+                    : props.transcription}</td>
+                <td className="table__data">{props.isSelected
+                    ? <Input placeholder="Введите перевод" />
+                    : props.russian}</td>
+                <td className="table__data"><div className='table_tags'>{props.isSelected
+                    ? <Input placeholder="Введите тэги" />
+                    : props.tags}</div></td>
+                <td className="table__data">
+                    {props.isSelected
+                        && <Button className=" btn btn_save" type="default" shape="circle" size="large">
+                            Save
+                        </Button>}
+                    <Button className="btn btn_edit" type="default" shape="circle" size="large">
+                        Edit
+                    </Button>
+                    <Button className="btn btn_del" type="danger " shape="circle" size="large">
+                        Del
+                    </Button>
+                </td>
+            </tr>
+        </tbody>
     )
 }
-
 export default TableOfWords;
