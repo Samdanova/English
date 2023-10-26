1201b5820e0041af9476d31d858071d9969dbd42
Samdanova <samdanova123@gmail.com>
2023-10-26 22:42:58 +0300

    loader

--- src/assets/components/Loader.jsx
+++ src/assets/components/Loader.jsx
@@ -1,7 +1,8 @@
-import { Spin } from 'antd';
+import { Space, Spin } from 'antd';
 
 export default function Loader() {
     return (
         < Spin tip="Loading..." className="spinLoading" />
+
     )
 }
